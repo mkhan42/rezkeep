5edff78985ae983e7cd5f33845ee2affcf402e0a
mkhan42 <michallekhan9@gmail.com>
2022-08-01 15:33:56 -0400

    fixed more styling

--- frontend/src/components/UserInfo/UserInfo.jsx
+++ frontend/src/components/UserInfo/UserInfo.jsx
@@ -13,8 +13,8 @@ function clickNew() {
     <div className="move-down">
     <div className="card">
     <div className="card-body">
-      <h5 className="card-title">Hello, {user.username}</h5>
-      <p>Welcome to RezKeeper, a place where you can keep track of all the restaurants you've visited</p>
+      <h4 className="card-title my-home-title">Hello, {user.username}</h4>
+      <p>Welcome to <span className='app-name'>RezKeeper</span>, a place where you can keep track of all the restaurants you've visited</p>
       <img className="card-img-top" src="https://media2.giphy.com/media/QZOyrhjVgZDR8TXVOg/giphy.gif?cid=ecf05e47jz2h44rfzkgjijdbgdpfi9o0gljczfxqn9jwx22x&rid=giphy.gif&ct=g" alt="Card image cap" />
       <div>
       <button className='btn' onClick={clickVisits}>Go to upcoming visits</button>
