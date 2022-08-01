e47738000d8f7172bd788203a6dc03dd1a6f1379
mkhan42 <michallekhan9@gmail.com>
2022-08-01 16:38:24 -0400

    fixed navbar buttons

--- frontend/src/components/UserInfo/UserInfo.jsx
+++ frontend/src/components/UserInfo/UserInfo.jsx
@@ -14,7 +14,9 @@ function clickNew() {
     <div className="card my-home-card">
     <div className="card-body">
       <h4 className="card-title my-home-title">Hello, {user.username}</h4>
+      <br/>
       <p className='my-home-p'>Welcome to <span className='app-name'>RezKeeper</span>, a place where you can keep track of all the restaurants you've visited</p>
+      <br/>
       <img className="card-img-top" src="https://media2.giphy.com/media/QZOyrhjVgZDR8TXVOg/giphy.gif?cid=ecf05e47jz2h44rfzkgjijdbgdpfi9o0gljczfxqn9jwx22x&rid=giphy.gif&ct=g" alt="Card image cap" />
       <div>
       <br></br>
